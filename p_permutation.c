a3c2623277d16a9bae61fcdb17fdf05ccf285640
Pire <p.clement01@orange.fr>
2023-04-28 14:56:15 +0200

    permutatione

--- p_permutation.c
+++ p_permutation.c
@@ -114,14 +114,92 @@ t_boolean est_permutation_valide(t_permutation ceci){
 
 t_boolean est_inferieur_strict(t_permutation ceci, t_permutation cela);
 
-t_boolean est_point_fixe_permutation(t_permutation ceci, int position);
+t_boolean est_point_fixe_permutation(t_permutation ceci, int position){
+    if (!est_p_permutation_initialise()) {
+        return FALSE;
+    }
+    if (est_indice_valide(position)) {
+        if (ceci[position] == position) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
 
-int nb_point_fixe(t_permutation ceci);
+int nb_point_fixe(t_permutation ceci){
+    if (!est_p_permutation_initialise()) {
+        return FALSE;
+    }
+    int nb_point_fixe = 0;
+    for (int i = 0; i < taille_permutation; i++) {
+        if (est_point_fixe_permutation(ceci, i)) {
+            nb_point_fixe++;
+        }
+    }
+    return nb_point_fixe;
+}
 
-t_boolean suivante_permutation(t_permutation ceci);
+t_boolean suivante_permutation(t_permutation ceci){
+    if (!est_p_permutation_initialise()) {
+        return FALSE;
+    }
+    int i = taille_permutation - 2;
+    while (i >= 0 && ceci[i] > ceci[i + 1]) {
+        i--;
+    }
+    if (i < 0) {
+        return FALSE;
+    }
+    int j = taille_permutation - 1;
+    while (ceci[i] > ceci[j]) {
+        j--;
+    }
+    echanger_tableau(ceci, i, j);
+    int k = i + 1;
+    int l = taille_permutation - 1;
+    while (k < l) {
+        echanger_tableau(ceci, k, l);
+        k++;
+        l--;
+    }
+    return TRUE;
+}
 
-t_boolean copier_suivante_permutation(t_permutation ceci, t_permutation cela);
+t_boolean copier_suivante_permutation(t_permutation ceci, t_permutation cela){
+    if (!est_p_permutation_initialise()) {
+        return FALSE;
+    }
+    copier_permutation(ceci, cela);
+    return suivante_permutation(cela);
+}
 
-t_boolean code_point_fixe_permutation(t_permutation ceci, int point_fixe, t_permutation cela);
+t_boolean code_point_fixe_permutation(t_permutation ceci, int point_fixe, t_permutation cela){
+    if (!est_p_permutation_initialise()) {
+        return FALSE;
+    }
+    if (est_indice_valide(point_fixe)) {
+        if (est_point_fixe_permutation(ceci, point_fixe)) {
+            for (int i = 0; i < taille_permutation; i++) {
+                cela[i] = ceci[i];
+            }
+            echanger_tableau(cela, point_fixe, taille_permutation - 1);
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
 
-t_boolean decode_point_fixe_permutation(t_permutation ceci, t_permutation cela, int* point_fixe);
+t_boolean decode_point_fixe_permutation(t_permutation ceci, t_permutation cela, int* point_fixe){
+    if (!est_p_permutation_initialise()) {
+        return FALSE;
+    }
+    if (est_permutation_valide(ceci)) {
+        for (int i = 0; i < taille_permutation; i++) {
+            cela[i] = ceci[i];
+        }
+        *point_fixe = cela[taille_permutation - 1];
+        echanger_tableau(cela, cela[taille_permutation - 1], taille_permutation - 1);
+        return TRUE;
+    }
+    return FALSE;
+}
